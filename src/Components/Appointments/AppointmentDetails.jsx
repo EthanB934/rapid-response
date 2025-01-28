aa03e58b11e3d5f2e00ee115285d0f3b6f59aa36
Ethan Brown <eb934053@gmail.com>
2025-01-28 11:30:21 -0600

    Added Appointment Details component

--- src/Components/Appointments/AppointmentDetails.jsx
+++ src/Components/Appointments/AppointmentDetails.jsx
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 import { getAppointmentsByAppointmentId } from "../../Services/AppointmentServices";
 import { getVisitorByUserId } from "../../Services/UserServices";
 
-export const AppointmentDetails = ({currentUser}) => {
+export const AppointmentDetails = ({ currentUser }) => {
   const [appointment, setAppointment] = useState({});
-  const [visitor, setVisitor] = useState({})
+  const [visitor, setVisitor] = useState({});
   const { appointmentId } = useParams();
 
   useEffect(() => {
@@ -22,5 +22,34 @@ export const AppointmentDetails = ({currentUser}) => {
     });
   }, [currentUser]);
 
-  return <>{appointment.id} {}</>;
+  return (
+    <>
+      {appointment.practitioner ? (
+        <>
+          <h1>Appointment Details for Appointment #{appointment.id}</h1>
+          <p>
+            Your have scheduled this appointment for the following reason:{" "}
+            {appointment.reason}. You will be meeting with our trusted clinical
+            expert {appointment.practitioner.fullName}. Doctor{" "}
+            {appointment.practitioner.fullName} practices{" "}
+            {appointment.practitioner.practice.map((practice) => {
+              return <>{practice}</>;
+            })}
+            . He has {appointment.practitioner.experience} years of experience
+            as a doctor.
+          </p>
+          <label>
+            You have scheduled this appointment for {appointment.scheduledDate}
+          </label>{" "}
+          {appointment.status ? (
+            <span>This appointment has been completed. We hope the visit went well!</span>
+          ) : (
+            <span>This appointment is still pending. We hope to see you soon!</span>
+          )}
+        </>
+      ) : (
+        "Waiting for practitioner"
+      )}
+    </>
+  );
 };
