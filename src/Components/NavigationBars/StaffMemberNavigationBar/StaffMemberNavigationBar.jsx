81a6bfc21e280100bcc55f1ce4d85f2a197b82f8
Ethan Brown <eb934053@gmail.com>
2025-01-28 14:39:53 -0600

    Added routing and navigation for practitioner users

--- src/Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar.jsx
+++ src/Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar.jsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom"
 
 export const StaffMemberNavigationBar = () => {
     return <>
+    <>
+    <Link to="/">Home</Link>
     <Link to="profile">My Profile</Link>
+    <Link to="appointments">My Appointments</Link>
+    <Link to="meetthestaff">Meet the Staff</Link>
+    </>
     </>
 }
--- src/Views/StaffMemberViews.jsx
+++ src/Views/StaffMemberViews.jsx
@@ -1,5 +1,48 @@
-import { StaffMemberNavigationBar } from "../Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar"
+import { Outlet, Route, Routes } from "react-router-dom";
+import { StaffMemberNavigationBar } from "../Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar";
+import { MyAppointments } from "../Components/Appointments/MyAppointments";
+import { AppointmentDetails } from "../Components/Appointments/AppointmentDetails";
+import { MeetTheStaff } from "../Components/Meet the Staff/MeetTheStaff";
+import { StaffDetails } from "../Components/Meet the Staff/StaffDetails";
 
-export const StaffMemberViews = () => {
-    return <StaffMemberNavigationBar/>
-}
+export const StaffMemberViews = ({currentUser}) => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <StaffMemberNavigationBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="/" element={<>welcome</>} />
+        <Route
+              path="create"
+              element={<>Profile</>}
+            />
+        <Route path="appointments">
+          <Route index element={<MyAppointments currentUser={currentUser} />} />
+          <Route path=":appointmentId">
+            <Route
+              index
+              element={<AppointmentDetails currentUser={currentUser} />}
+            />
+            <Route
+              path="edit"
+              element={<>Profile</>}
+            />
+          </Route>
+        </Route>
+        <Route path="meetthestaff">
+          <Route index element={<MeetTheStaff />} />
+          <Route
+            path=":practitionerId"
+            element={<StaffDetails currentUser={currentUser} />}
+          />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
