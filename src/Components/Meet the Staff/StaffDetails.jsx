065b73df3028f1d2e4e022121fbc50aa8d7e749b
Ethan Brown <eb934053@gmail.com>
2025-01-28 14:06:41 -0600

    Displayed all necessary information for meet the staff list items

--- src/Components/Meet the Staff/StaffDetails.jsx
+++ src/Components/Meet the Staff/StaffDetails.jsx
@@ -3,14 +3,35 @@ import { useParams } from "react-router-dom";
 import { getProfileByPractitionerId } from "../../Services/PractitionerServices";
 
 export const StaffDetails = () => {
-  const [practitioner, setPractitioner] = useState({});
+  const [profile, setProfile] = useState({});
   const { practitionerId } = useParams();
 
   useEffect(() => {
-    getProfileByPractitionerId(practitionerId).then((practitionerObject) =>
-      setPractitioner(practitionerObject)
-    );
+    getProfileByPractitionerId(practitionerId).then((responseArray) => {
+      const practitionerProfile = responseArray[0];
+      setProfile(practitionerProfile);
+    });
   }, [practitionerId]);
 
-  return <>Here are the staff details</>;
+  return (
+    <>
+      {profile.practitioner ? (
+        <article>
+          <section>
+            <p>
+              My name is {profile.practitioner.fullName}, I am{" "}
+              {profile.practitioner.age} years young. I have been working in my
+              dedicated field(s),{" "}
+              {profile.practitioner.practice.map((practice) => {
+                return <>{practice}</>;
+              })}{", "}
+              for {profile.practitioner.experience} years. Here is a little bit about me, outside of work. {profile.bio}
+            </p>
+          </section>
+        </article>
+      ) : (
+        <>{"Waiting for details..."}</>
+      )}
+    </>
+  );
 };
