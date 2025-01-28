155218204ec35bb6860c8a16048e8d7eb9886830
Ethan Brown <eb934053@gmail.com>
2025-01-28 13:41:45 -0600

    Added Meet the Staff Component, listing all staff members

--- src/Components/Meet the Staff/MeetTheStaff.jsx
+++ src/Components/Meet the Staff/MeetTheStaff.jsx
@@ -1,3 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getAllPractitioners } from "../../Services/PractitionerServices";
+import { Link } from "react-router-dom";
+
 export const MeetTheStaff = () => {
-    return <>This is where the staff members and their associated information will be listed</>
-}
+  const [practitioners, setPractitioners] = useState([]);
+
+  useEffect(() => {
+    getAllPractitioners().then((practitionersArray) =>
+      setPractitioners(practitionersArray)
+    );
+  }, []);
+
+  return (
+    <>
+      {practitioners.map((practitioner) => {
+        return (
+          <>
+            <Link to={`/practitioner/${practitioner.id}`} key={practitioner.id}>
+              {practitioner.fullName}
+            </Link>
+            {practitioner.practice.map((practice) => {
+                return <>{practice}</>
+            })}
+            {practitioner.experience}
+          </>
+        );
+      })}
+    </>
+  );
+};
