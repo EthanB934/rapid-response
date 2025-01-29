c9137d877d7b79e383419094bf0f725c14aeaaa2
Ethan Brown <eb934053@gmail.com>
2025-01-29 14:12:39 -0600

    Styled Navigation Bar, added logout functionality

--- src/Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar.jsx
+++ src/Components/NavigationBars/StaffMemberNavigationBar/StaffMemberNavigationBar.jsx
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
-
+import "../NavigationBars.css";
 export const StaffMemberNavigationBar = ({ profile }) => {
   return (
-    <>
-      <Link to="/">Home</Link>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
       {profile?.practitioner ? (
-        <Link to={`/meetthestaff/${profile.practitionerId}`}>My Profile</Link>
+        <li>
+          <Link to={`/meetthestaff/${profile.practitionerId}`}>My Profile</Link>
+        </li>
       ) : (
-        <Link to="profile">My Profile</Link>
+        <li>
+          <Link to="profile">My Profile</Link>
+        </li>
       )}
-      <Link to="appointments">My Appointments</Link>
-      <Link to="meetthestaff">Meet the Staff</Link>
-    </>
+      <li>
+        <Link to="appointments">My Appointments</Link>
+      </li>
+      <li>
+        <Link to="meetthestaff">Meet the Staff</Link>
+      </li>
+      <li>
+        <Link
+          to=""
+          onClick={() => {
+            localStorage.removeItem("rapidResponse_user");
+            navigate("/", { replace: true });
+          }}
+        >
+          Logout
+        </Link>
+      </li>
+    </ul>
   );
 };
--- src/Components/NavigationBars/VisitorNavigationBar/VisitorNavigationBar.jsx
+++ src/Components/NavigationBars/VisitorNavigationBar/VisitorNavigationBar.jsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom";
+import "../NavigationBars.css";
 
 export const VisitorNavigationBar = () => {
   return (
-    <>
-    <Link to="/">Home</Link>
-    <Link to="create">Create an Appointment</Link>
-    <Link to="appointments">My Appointments</Link>
-    <Link to="meetthestaff">Meet the Staff</Link>
-    </>
+    <ul className="nav">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="create">Create an Appointment</Link>
+      </li>
+      <li>
+        <Link to="appointments">My Appointments</Link>
+      </li>
+      <li>
+        <Link to="meetthestaff">Meet the Staff</Link>
+      </li>
+      <li>
+        <Link
+          to=""
+          onClick={() => {
+            localStorage.removeItem("rapidResponse_user");
+            navigate("/", { replace: true });
+          }}
+        >
+          Logout
+        </Link>
+      </li>
+    </ul>
   );
 };
