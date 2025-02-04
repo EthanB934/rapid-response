1ad89380aedb90abea3980323ec95ff7b45ec934
Ethan Brown <eb934053@gmail.com>
2025-02-04 09:32:04 -0600

    Added docstrings to My Appointments Component

--- src/Components/Appointments/MyAppointments.jsx
+++ src/Components/Appointments/MyAppointments.jsx
@@ -13,7 +13,7 @@ export const MyAppointments = ({ currentUser }) => {
   const [appointments, setAppointments] = useState([]);
   const [visitor, setVisitor] = useState({});
   const [appointmentStatus, setAppointmentStatus] = useState(false);
-
+  // Visitors and Practitioners require this component. Filters appointments for specific user and their role.
   useEffect(() => {
     if (!currentUser.isStaff) {
       getVisitorByUserId(currentUser.id).then((responseArray) => {
@@ -28,8 +28,12 @@ export const MyAppointments = ({ currentUser }) => {
       });
     }
   }, [currentUser]);
+  // UseEffect sets state variable, visitor; a generic variable that can represent either a visitor or a practitioner
 
+  // Get and Set function definition that sets state variable
   const getAndSetAppointments = () => {
+    // Depends on boolean value
+    // Depends on useEffect setting visitor state variable
     if (!currentUser.isStaff) {
       getAppointmentsByVisitorId(visitor?.id).then((responseArray) => {
         setAppointments(responseArray);
@@ -41,22 +45,28 @@ export const MyAppointments = ({ currentUser }) => {
       });
     }
   };
+
   useEffect(() => {
     getAndSetAppointments();
   }, [visitor]);
+  // useEffect observes visitor state variable. Runs when state changes.
 
+  // Visible only to visitors
+  // When an appointment's status is complete, this function is associated with the rendered button.
   const handleRemoveAppointment = (event) => {
     const appointmentId = parseInt(event.target.value);
     removeAppointment(appointmentId).then(getAndSetAppointments);
+    // If the button is clicked, the associated appointment is removed. Then the list is re-fetched for the updated list.
   };
 
+  // Visible only to practitioners
+  // Associated function that allows a practitioner to update an appointment's status from false to true.
   const handleAppointmentCompletion = (event) => {
     const appointmentId = parseInt(event.target.value);
-    // debugger
     const chosenAppointmentToComplete = appointments.filter(
       (appointment) => appointment.id === appointmentId
     );
-
+    // JSON body to be sent via "PUT" request
     const appointmentFormForStatusUpdate = {
       id: appointmentId,
       reason: chosenAppointmentToComplete[0].reason,
@@ -65,24 +75,28 @@ export const MyAppointments = ({ currentUser }) => {
       practitionerId: visitor.id,
       completed: true,
     };
+    // Invokes "PUT" request function.
     updateAppointmentDetails(appointmentFormForStatusUpdate).then(
       getAndSetAppointments
     );
-    // completeAppointment(appointmentId)
+    // After "PUT" request is successful, Get and Set runs to reflect updated appointment status; button is no longer visible.
   };
 
   return (
     <section className="list">
       <div className="list list2">
+        {/* Waits for associated appointments relevant to current user to load. */}
         {appointments ? (
           <>
             {appointments.map((appointment) => {
               return (
                 <div className="listCard">
-                  <Link className="ink" to={`/appointments/${appointment.id}`}>
+                  {/* Link associated with each appointment reason. Redirects user to that appointment's details. */}
+                  <Link className="link" to={`/appointments/${appointment.id}`}>
                     {appointment.reason}
                   </Link>{" "}
                   {appointment.scheduledDate}{" "}
+                  {/* Begins conditional rendering depending on two booleans */}
                   {!currentUser.isStaff && appointment.completed === true ? (
                     <>
                       <div>
@@ -109,8 +123,7 @@ export const MyAppointments = ({ currentUser }) => {
                   {currentUser.isStaff && appointment.completed === true ? (
                     <>
                       {"This appointment has been marked as completed"}
-                      <div className="appointmentStatus">
-                      </div>
+                      <div className="appointmentStatus"></div>
                     </>
                   ) : (
                     " "
@@ -120,11 +133,11 @@ export const MyAppointments = ({ currentUser }) => {
                       {"This appointment has not been marked as completed"}
                       <div className="appointmentStatus"></div>
                       <button
-                          value={appointment.id}
-                          onClick={handleAppointmentCompletion}
-                        >
-                          Complete
-                        </button>
+                        value={appointment.id}
+                        onClick={handleAppointmentCompletion}
+                      >
+                        Complete
+                      </button>
                     </>
                   ) : (
                     " "
@@ -134,6 +147,7 @@ export const MyAppointments = ({ currentUser }) => {
             })}
           </>
         ) : (
+          // Fallback if appointments has not yet loaded, virtually unseen.
           <>Loading...</>
         )}
       </div>
